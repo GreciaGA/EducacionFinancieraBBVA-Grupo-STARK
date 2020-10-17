885174e8fa966574cddbf72294b772108e6d0cd9
GreciaGA <grecia.anicama@gmail.com>
2020-10-17 17:33:30 -0500

    Subiendo home y asistentes financieros

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const asistenteAlita = document.getElementById('asistente-alita');
+const asistenteDalton = document.getElementById('asistente-dalton');
+
+asistenteAlita.addEventListener('click', ()=>{
+
+})
