0bebc1d351519816ca991b253f222bb7f461d228
GreciaGA <grecia.anicama@gmail.com>
2020-10-17 22:47:17 -0500

    Subiendo form unete a BBVA-2

--- js/quieroestarasegurado.js
+++ js/quieroestarasegurado.js
@@ -1,6 +1,7 @@
 
 const btnRegresar = document.getElementById('btnRegresar');  
 btnRegresar.addEventListener('click',()=>{
-    window.open("../index.html");
+    // window.open("../index.html");
+    window.open("https://greciaga.github.io/EducacionFinancieraBBVA-Grupo-STARK/");
     console.log("regresar page");
 })
